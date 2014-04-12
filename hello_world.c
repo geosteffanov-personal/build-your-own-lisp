3cb7334aaf474277fea7a14a15603d2716f61954
Nick Payne <slashnick@gmail.com>
2014-04-12 17:06:43 +0100

    Chapter 3: I can loop

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+
+void say_hello(int count) {
+    while(count > 0) {
+        puts("Hello, World!");
+        count--;
+    }
+}
+
 int main(int argc, char** argv) {
-    puts("Hello, world!" );
+    say_hello(5);
     return 0;
 }
