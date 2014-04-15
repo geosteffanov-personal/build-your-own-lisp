972ca4df934ea89753c0d85ed01b44664b03d2c6
Nick Payne <slashnick@gmail.com>
2014-04-15 19:36:08 +0100

    Chapter 9: Change grammar to be more lispy

--- parsing.c
+++ parsing.c
@@ -79,20 +79,22 @@ lval eval(mpc_ast_t* t) {
 }
 
 int main(int argc, char** argv) {
-    /* Create som parsers */
+    /* Create some parsers */
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispy = mpc_new("lispy");
 
     mpca_lang(MPC_LANG_DEFAULT,
         "\
             number : /-?[0-9]+/; \
-            operator : '+' | '-' | '*' | '/'; \
-            expr : <number> | '(' <operator> <expr>+ ')' ; \
-            lispy : /^/ <operator> <expr>+ /$/ ; \
+            symbol : '+' | '-' | '*' | '/'; \
+            sexpr : '(' <expr>* ')' ; \
+            expr : <number> | <symbol> | <sexpr> ; \
+            lispy : /^/ <expr>* /$/ ; \
         ",
-        Number, Operator, Expr, Lispy);
+        Number, Symbol, Sexpr, Expr, Lispy);
 
     /* Print version and exit information */
     puts("Lispy Version 0.0.0.0.1");
@@ -130,5 +132,6 @@ int main(int argc, char** argv) {
         free(input);
     }
 
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
     return 0;
 }
