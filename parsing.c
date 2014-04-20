7df43a7ca275e2aea2e6a0d01605b0530aab5c88
Nick Payne <slashnick@gmail.com>
2014-04-20 13:53:14 +0100

    Chapter 9: An s-expressiony calculator

--- parsing.c
+++ parsing.c
@@ -70,6 +70,7 @@ lval* lval_add(lval* v, lval* x) {
 }
 
 void lval_print(lval* v);
+lval* lval_eval(lval* v);
 
 void lval_expr_print(lval* v, char open, char close) {
     putchar(open);
@@ -93,6 +94,107 @@ void lval_print(lval* v) {
 }
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
+
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at the given index */
+    lval* x = v->cell[i];
+
+    /* Shift the memory following the item at "i" over the top of it */
+    memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v-> cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+
+    /* Ensure all arguments are numbers */
+    for (int i =0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot apply operator to non number");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and is a subtract operation then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) { x->num = -x->num; }
+
+    while (a->count > 0) {
+        lval* y = lval_pop(a, 0);
+
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if(y->num == 0) {
+                lval_del(x);
+                lval_del(y);
+                x = (lval_err("Divisiion by zero")); break;
+            }
+            x->num /= y->num;
+        }
+
+        /* Delete element now finished with it */
+        lval_del(y);
+    }
+
+    /* Delete input expression and return result */
+    lval_del(a);
+    return x;
+}
+        
+lval* lval_eval_sexpr(lval* v) {
+
+    /* Evaluate children */
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Error checking */
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    /* Empty expression */
+    if (v->count == 0) { return v; }
+
+    /* Single expression */
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    /* Ensure first element is a symbol */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f);
+        lval_del(v);
+        return lval_err("S-expression does not start with a symbol");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    /* Evalute s-expressions */
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    /* Just return anything else as is */
+    return v;
+}
 
 lval* lval_read_num(mpc_ast_t* t) {
     errno = 0;
@@ -152,8 +254,8 @@ int main(int argc, char** argv) {
         /* Echo back to use */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
-            lval* x = lval_read(r.output);
+            //mpc_ast_print(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
             mpc_ast_delete(r.output);
